d134ca9cc246e9ac03dbd8959457e2e9abdeeeb5
Federico <fede.pernuzzi@gmail.com>
2022-09-12 19:13:11 -0300

    Sección home, background y estilos

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+import Background from '../img/home1.jpg'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{backgroundImage: `url(${Background})`}} className='w-screen h-screen bg-no-repeat bg-contain lg:bg-cover'></div>
+  )
+}
+
+export default Home
