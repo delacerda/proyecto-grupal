ba683db2111bfad039d8e366e5b3f8deb9213f99
Ambar Muller <apalaciom30@gmail.com>
2022-09-14 23:02:03 -0300

    sobre nosotros section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import Fixture from "./components/Fixture/Fixture";
 import Footer from "./components/Footer";
+import SobreNosotros from "./components/sobreNosotros/SobreNosotros";
+
 
 
 function App() {
   return (
     <>
+    <SobreNosotros />
     <Fixture/>
     <Footer/>
     </>
