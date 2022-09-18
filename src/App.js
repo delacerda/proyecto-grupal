70a7a4fd33d644302f99666da3e5510e31d4d0e3
Federico <fede.pernuzzi@gmail.com>
2022-09-18 16:22:29 -0300

    Countdown creado y funcionando

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import ListaFiguritas from "./components/ListaFiguritas";
 import Fixture from "./components/Fixture/Fixture";
 import Footer from "./components/Footer";
 import SobreNosotros from "./components/sobreNosotros/SobreNosotros";
+import Countdown from "./components/Countdown/Countdown";
 
 function App() {
   return (
     <>
       <Home />
+      <Countdown countdownTimestampMs={1668970800000} />
       <ListaFiguritas />
       <SobreNosotros />
       <Fixture />
