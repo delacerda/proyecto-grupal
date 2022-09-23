593dd0822876eacc53b1a85cf9578c92087615d2
Federico <fede.pernuzzi@gmail.com>
2022-09-23 20:03:41 -0300

    Nuevo Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
-import Background from "../img/home1.jpg";
+import Background from "../img/home2.jpg";
 import React from "react";
 
 const Home = () => {
   return (
     <div
       style={{ backgroundImage: `url(${Background})` }}
-      className="w-screen h-screen bg-no-repeat bg-bottom-left bg-cover"
+      className="w-screen h-screen bg-no-repeat bg-bottom-left bg-cover bg-center "
     >
     </div>
   );
